b730c750dcdd1f61eb31f6e7b9f39a078826ca75
Kaushal Pareek <kpareek2592@gmail.com>
2018-01-07 22:47:32 +0530

    Add project files.

---
+++ WebAPI Demo/Controllers/EmployeesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI_Demo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity =  entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity != null)
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    else
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id = " + id.ToString() + " not found.");
+                }           
+        }
+
+        public HttpResponseMessage Post([FromBody]Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+            
+
+            
+        }
+    }
+}
