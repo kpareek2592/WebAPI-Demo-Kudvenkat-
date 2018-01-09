935755ae4a6846a687266be61ac4c4ef7a58118b
Kaushal Pareek <Kaushal.Pareek@mphasis.com>
2018-01-09 23:21:42 +0530

    Implemented Put method in EmployeesController

--- WebAPI Demo/Controllers/EmployeesController.cs
+++ WebAPI Demo/Controllers/EmployeesController.cs
@@ -19,14 +19,14 @@ namespace WebAPI_Demo.Controllers
 
         public HttpResponseMessage Get(int id)
         {
-                using (EmployeeDBEntities entities = new EmployeeDBEntities())
-                {
-                    var entity =  entities.Employees.FirstOrDefault(e => e.ID == id);
-                    if (entity != null)
-                        return Request.CreateResponse(HttpStatusCode.OK, entity);
-                    else
-                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id = " + id.ToString() + " not found.");
-                }           
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                if (entity != null)
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                else
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id = " + id.ToString() + " not found.");
+            }
         }
 
         public HttpResponseMessage Post([FromBody]Employee employee)
@@ -71,6 +71,37 @@ namespace WebAPI_Demo.Controllers
                 {
                     return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
                 }
+            }
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]Employee employee)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                try
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee not found with id = " + id.ToString());
+                    }
+                    else
+                    {
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entity.Gender = employee.Gender;
+                        entity.Salart = employee.Salart;
+
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee not found with id = " + id.ToString()); ;
+                }
+                
                 
             }
         }
